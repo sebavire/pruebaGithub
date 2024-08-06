6eedf215e3b0641997f9d6774cb6a8630a66eee8
sebavire <sebastian.vire@gmail.com>
2024-08-06 09:13:18 -0300

    primer commit

---
+++ src/main/java/com/mycompany/respaldo_github/Respaldo_github.java
@@ -0,0 +1,10 @@
+package com.mycompany.respaldo_github;
+
+
+public class Respaldo_github {
+
+    public static void main(String[] args) {
+            Ventana unaVentana = new Ventana();
+            unaVentana.setVisible(true);
+    }
+}
